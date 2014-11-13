036129a3b48c2ee67eabdecd79d78bd372bfd6cf
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-11-12 22:26:12 -0700

    Create plugin entry file.

---
+++ wpslc-mexp-demo.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: WP-SLC Meetup Media Explorer Demo
+ * Description: An example implementation of a new service for the Media Explorer.
+ * Author: Jeff Stieler
+ * Author URI: http://voceplatforms.com/
+ * License: GPLv2 or later
+ * Requires at least: 3.6
+ * Version: 0.1
+ */
+
+require( 'mexp-plugin.php' );
