076bfa1ca1ba52a76c5400c5b089a80d512bcb9c
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-11-15 23:44:17 -0700

    Fix featured video retrieval and only show iframe if there’s one set.

--- featured-youtube-video.php
+++ featured-youtube-video.php
@@ -42,7 +42,7 @@ class Featured_Youtube_Video {
 	 */
 	function display_meta_box( $post, $box ) {
 
-		$featured_video      = get_post_meta( $post->id, 'featured-youtube-video', true );
+		$featured_video      = get_post_meta( $post->ID, 'featured-youtube-video', true );
 		$set_link_classes    = array( 'set-featured-youtube-video' );
 		$remove_link_classes = array( 'remove-featured-youtube-video' );
 
@@ -59,7 +59,9 @@ class Featured_Youtube_Video {
 		?>
 		<input type="hidden" name="featured-youtube-video" id="featured-youtube-video-url" value="<?php echo esc_url( $featured_video ); ?>" />
 		<div class="featured-youtube-video-preview">
+		<?php if ( $featured_video ) : ?>
 			<iframe width="475" height="265" frameborder="0" allowfullscreen src="<?php echo str_replace( 'watch?v=', 'embed/', esc_url( $featured_video ) ); ?>"></iframe>
+		<?php endif; ?>
 		</div>
 		<a href="#" class="<?php echo implode( ' ', $set_link_classes ); ?>">Set featured Youtube video</a>
 		<a href="#" class="<?php echo implode( ' ', $remove_link_classes ); ?>">Remove featured Youtube video</a>
